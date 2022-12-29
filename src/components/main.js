f5e95bd24174c23bb90c813f38b22369c1f13635
James Pan <jtrpan@gmail.com>
2022-12-29 18:51:13 -0500

    Landing page particle fix

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Navigate, Route } from 'react-router-dom';
 
 import LandingPage from './landingpage';
 import AboutMe from './about/aboutme';
@@ -7,12 +7,14 @@ import Contact from './contact/contact';
 import Projects from './projects/projects';
 
 const Main = () => (
-    <Switch>
-        <Route exact path="/" component={LandingPage}/>
-        <Route path="/aboutme" component={AboutMe}/>
-        <Route path="/contact" component={Contact}/>
-        <Route path="/projects" component={Projects}/>
-    </Switch>
+    <Routes>
+        <Route exact path="/" element={<LandingPage/>}/>
+        <Route path="/aboutme" element={<AboutMe/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/*" element={<Navigate to="/" replace />} />
+    </Routes>
 )
 
 export default Main;
---
+++ src/components/particles/LandingParticles.js
@@ -0,0 +1,87 @@
+import Particles from "react-tsparticles";
+import {loadSlim} from "tsparticles-slim"; // loads tsparticles-slim
+//import { loadFull } from "tsparticles"; // loads tsparticles
+import {useCallback, useMemo} from "react";
+import {loadFull} from "tsparticles";
+
+// tsParticles Repository: https://github.com/matteobruni/tsparticles
+// tsParticles Website: https://particles.js.org/
+const ParticlesComponent = (props) => {
+    // using useMemo is not mandatory, but it's recommended since this value can be memoized if static
+    const options = useMemo(() => {
+        // using an empty options object will load the default options, which are static particles with no background and 3px radius, opacity 100%, white color
+        // all options can be found here: https://particles.js.org/docs/interfaces/Options_Interfaces_IOptions.IOptions.html
+        return {
+            retina_detect: true,
+            fpsLimit: 200,
+            fullScreen: {
+                enable: true, // enabling this will make the canvas fill the entire screen, it's enabled by default
+            }, interactivity: {
+                detect_on: "canvas",
+                events: {
+                    onhover: {
+                        enable: true, mode: "grab"
+                    }, onclick: {
+                        enable: true, mode: "push"
+                    }, resize: true,
+                }, modes: {
+                    push: {
+                        quantity: 4, // number of particles to add on click
+                    }, grab: {
+                        distance: 300, // distance of the particles from the cursor
+                    },
+                },
+            }, particles: {
+                links: {
+                    enable: true, // enabling this will make particles linked together
+                    distance: 200, // maximum distance for linking the particles
+                }, move: {
+                    enable: true, // enabling this will make particles move in the canvas
+                    speed: {min: 1, max: 5}, // using a range in speed value will make particles move in a random speed between min/max values, each particles have its own value, it won't change in time by default
+                }, opacity: {
+                    value: {min: 0.3, max: 0.7}, // using a different opacity, to have some semitransparent effects
+                }, size: {
+                    value: {min: 1, max: 3}, // let's randomize the particles size a bit
+                },
+            },
+        };
+    }, []);
+
+    // useCallback is not mandatory, but it's recommended since this callback can be memoized if static
+    const particlesInit = useCallback((engine) => {
+        loadFull(engine);
+        // loadFull(engine); // for this sample the slim version is enough, choose whatever you prefer, slim is smaller in size but doesn't have all the plugins and the mouse trail feature
+    }, []);
+
+    // setting an id can be useful for identifying the right particles component, this is useful for multiple instances or reusable components
+    return <Particles id={props.id} init={particlesInit} options={options}/>;
+
+    /*
+    <Particles
+                id="landingparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                width={this.state.windowWidth}
+                height={this.state.windowHeight}
+                params={{
+                    fpsLimit: 200, particles: {
+                        number: {
+                            value: this.state.windowWidth * this.state.windowHeight / 15500,
+                        }, size: {
+                            value: 2
+                        }
+                    }, interactivity: {
+                        detect_on: "canvas", events: {
+                            onhover: {
+                                enable: true, mode: "grab"
+                            }, onclick: {
+                                enable: true, mode: "push"
+                            }, resize: true
+                        },
+                    }, "retina_detect": true
+                }}
+            />
+     */
+};
+
+export default ParticlesComponent;
